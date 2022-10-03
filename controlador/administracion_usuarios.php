ca60a22fc7d0b942b00f9beedcd0ad1a77909347
ralvarezl <ralvarezl@unah.hn>
2022-10-02 21:50:31 -0600

    Edicion del controlador administracion usuarios

--- controlador/administracion_usuarios.php
+++ controlador/administracion_usuarios.php
@@ -1,29 +1,30 @@
 <?php
 
-//Validacion Creacion de un nuevo usuario
-if (!empty($_POST["btnguardar"])){
-    //VALIDAR QUE TODOS LOS CAMPOS ESTEN RELLENADOS
-    if (!empty($_POST["nombres"]) and !empty($_POST["usuario"]) and !empty($_POST["password"]) and !empty($_POST["identidad"]) and !empty($_POST["telefono"]) and !empty($_POST["direccion"]) and !empty($_POST["correo"])) {
+//Validacion Creacion de un nuevo usuario*****************************************************
+if (!empty($_POST["btnregistrar"])) {
+    if (!empty($_POST["nombres"]) and !empty($_POST["usuario"]) and !empty($_POST["password"]) and !empty($_POST["identidad"]) and !empty($_POST["genero"]) and !empty($_POST["telefono"]) and !empty($_POST["direccion"]) and !empty($_POST["correo"]) and !empty($_POST["estado"]) and !empty($_POST["id_rol"])) {
         $nombres=$_POST["nombres"];
         $usuario=$_POST["usuario"];
         $password=$_POST["password"];
         $identidad=$_POST["identidad"];
+        $genero=$_POST["genero"];
         $telefono=$_POST["telefono"];
         $direccion=$_POST["direccion"];
         $correo=$_POST["correo"];
-                
-        date_default_timezone_set("America/Tegucigalpa");
-        $mifecha = date('Y-m-d');
-        //MANDAR LA INFORMACIÓN DE LOS CAMPOS
-        $sql=$conexion->query("INSERT INTO tbl_ms_usuario (nombres, usuario, password, identidad, telefono, direccion, correo, fecha_creacion) value ( '$nombres', '$usuario', '$password','$identidad', '$telefono', '$direccion', '$correo', '$mifecha')");
+        $estado=$_POST["estado"];
+        $rol=$_POST["id_rol"];
 
-        echo"<div class='alert alert-warning'>Usuario Creado Exitosamente</div>";//CREACIÓN EXITOSA
-              
+        $sql=$conexion->query(" insert into tbl_ms_usuario(nombres, usuario, password, identidad, genero, telefono, direccion, correo, estado, id_rol) values('$nombres', '$usuario', '$password', '$identidad', '$genero', '$telefono', '$direccion', '$correo', '$estado', '$rol')");
+        if ($sql==1) {
+            echo '<div class="alert alert-success">Usuario registrado correctamente</div>';
+        } else {
+            echo '<div class="alert alert-danger">Error al registrar usuario</div>';
+        }
         
-    }else{
-        echo"<div class='alert alert-warning'>Favor Rellenar Campos </div>";//TIENE CAMPOS VACIOS
+    } else {
+        echo '<div class="alert alert-warning">Favor llenar todos los campos</div>';
     }
-   
+    
 }
 
 ?>
--- vista/administracion/administracion_usuarios.php
+++ vista/administracion/administracion_usuarios.php
@@ -6,189 +6,114 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administracion Usuarios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/02575225aa.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="administracion_usuarios.css">
 
 </head>
 
 <body>
-    
-    <form action="administracion_usuarios.php" method="post">
-    <div class="container-fluid">
-        <div class="row justify-content-center align-content-center">
-            <div class="col-8 barra">
-                <h4 class="text-light">Logo</h4>
+    <h1 class="text-center p-3">USUARIOS</h1>
+    <div class="container-fluid row">
+        <form class="col-2 p-3" method="POST">
+            <h3 class="text-center text-secundary">REGISTRO DE USUARIOS</h3>
+            <?php
+            include "../../modelo/conexion.php";
+            include "../../controlador/administracion_usuarios.php";
+            ?>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nombres</label>
+            <input type="text" class="form-control" placeholder="Ingrese nombres" name="nombres">
             </div>
-            <div class="col-4 text-right barra">
-                <ul class="navbar-nav mr-auto">
-                    <li>
-                        <a href="#" class="px-3 text-light perfil dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-circle user"></i></a>
-
-                        <div class="dropdown-menu" aria-labelledby="navbar-dropdown">
-                            <a class="dropdown-item menuperfil cerrar" href="../../login.php"><i class="fas fa-sign-out-alt m-1"></i>Salir
-                            </a>
-                        </div>
-                    </li>
-                </ul>
+            <div class="mb-3">
+			<label for="formGroupExampleInput" class="form-label">Usuario</label>
+			<input id="usuario" type="text"
+				class="form-control" name="usuario" title="ingrese usuario" autocomplete="usuario" placeholder="Ingrese usuario" onKeyUp="document.getElementById(this.id).value=document.getElementById(this.id).value.toUpperCase()">
+		    </div>
+            <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" placeholder="Ingrese contraseña" name="password">
             </div>
-        </div>
-    </div>
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="barra-lateral col-12 col-sm-auto">
-                <nav class="menu d-flex d-sm-block justify-content-center flex-wrap">
-                    <a href="#"><i class="fas fa-home"></i><span>ADMINISTRACION DE USUARIOS</span></a>
-                    <a href="#"><i class="fas fa-home"></i><span>Inicio</span></a>
-                    <a href="#"><i class="fas fa-home"></i><span>Inicio</span></a>
-                    <a href="#"><i class="fas fa-home"></i><span>Inicio</span></a>
-                </nav>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Identidad</label>
+            <input type="text" class="form-control" placeholder="Ingrese numero de identidad" name="identidad">
             </div>
-            <main class="main col">
-                <div class="row justify-content-center align-content-center text-center">
-                    <div class="columna col-lg-6">
-                        <?php
-                        //Llamado a la base de datos
-                        include "../../modelo/conexion.php";
-                        //Consulta la tabla:
-                        $sql= $conexion->query("select id_usuario, nombres, usuario,password, identidad, telefono, estado, rol, correo from tbl_ms_usuario u join tbl_ms_roles r ON  r.ID_ROL=u.ID_ROL ");
-                        ?>
-                        <table class = "table table-dark table-striped">
-                            <h1>USUARIOS</h1>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Genero</label>
+            <input type="text" class="form-control" placeholder="Ingrese genero" name="genero">
+            </div>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Telefono</label>
+            <input type="text" class="form-control" placeholder="Ingrese telefono" name="telefono">
+            </div>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Direccion</label>
+            <input type="text" class="form-control" placeholder="Ingrese direccion" name="direccion">
+            </div>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Correo</label>
+            <input type="text" class="form-control" placeholder="Ingrese correo electronico" name="correo">
+            </div>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Estado</label>
+            <input type="text" class="form-control" placeholder="Ingrese estado del usuario" name="estado">
+            </div>
+            <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Rol</label>
+            <input type="text" class="form-control" placeholder="Ingrese rol del usuario" name="rol">
+            </div>
+            <!--BOTON NUEVO USUARIO-->
+            <button type="submit" class="btn btn-dark" name="btnregistrar" value="ok">Registrar Usuario</button>
+        </form>
+                <div class="col-8 p-4">
+                    <table class="table">
+                        <thead class="table-dark">
                             <tr>
-                                <td>ID</td>
-                                <td>NOMBRES</td>
-                                <td>USUARIO</td>
-                                <td>CONTRASEÑA</td>
-                                <td>IDENTIDAD</td>
-                                <td>TELEFONO</td>
-                                <td>ESTADO</td>
-                                <td>ROL</td>
-                                <td>CORREO</td>
-                        </tr>
-                            
-                        <?php while($u = $sql->fetch_assoc()){ ?>
+                            <th scope="col">ID</th>
+                            <th scope="col">NOMBRES</th>
+                            <th scope="col">USUARIO</th>
+                            <th scope="col">CONTRASEÑA</th>
+                            <th scope="col">IDENTIDAD</th>
+                            <th scope="col">GENERO</th>
+                            <th scope="col">TELEFONO</th>
+                            <th scope="col">DIRECCION</th>
+                            <th scope="col">CORREO</th>
+                            <th scope="col">ESTADO</th>
+                            <th scope="col">ROL</th>
+                            <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            //Llamado a la base de datos
+                            include "../../modelo/conexion.php";
+                            $sql= $conexion->query("select id_usuario, nombres, usuario, password, identidad, genero, telefono, direccion, correo, estado, rol from tbl_ms_usuario u join tbl_ms_roles r ON  r.ID_ROL=u.ID_ROL ");
+                            while($u = $sql->fetch_assoc()){ ?>
                             <tr>
                                 <td><?php echo $u['id_usuario']; ?></td>
                                 <td><?php echo $u['nombres']; ?></td>
                                 <td><?php echo $u['usuario']; ?></td>
                                 <td><?php echo $u['password']; ?></td>
                                 <td><?php echo $u['identidad']; ?></td>
+                                <td><?php echo $u['genero']; ?></td>
                                 <td><?php echo $u['telefono']; ?></td>
+                                <td><?php echo $u['direccion']; ?></td>
+                                <td><?php echo $u['correo']; ?></td>
                                 <td><?php echo $u['estado']; ?></td>
                                 <td><?php echo $u['rol']; ?></td>
-                                <td><?php echo $u['correo']; ?></td>
+                                <td>
+                                    <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-user-pen"></i></a>
+                                    <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash-can"></i></a>
+                                </td>
                             </tr>
-                        <?php } ?>
-                        </table>
-                        <!--BOTON PROVISIONAL SALIDA-->
-                        <input onclick="location.href='../../login.php'" name="" class="btn btn-danger" title="click para registrar un nuevo usuario" type="dark" value="Salir">
-
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                         Agregar Usuario
-                        </button>
-
-                        <!-- Modal Registro Usuario -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Registro Usuario</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-
-                            <!--DATOS DEL MODAL-->
-                            <?php
-                            //Llamado a la base de datos
-                            include "../../modelo/conexion.php";
-                            include "../../controlador/administracion_usuarios.php";  
+                            <?php }
                             ?>
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Nombres</label>
-                                <input id="nombres" type="text" class="form-control" placeholder="Ingrese nombres">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Usuario</label>
-                                <input id="usuario" type="text" class="form-control" placeholder="Ingrese usuario" onKeyUp="document.getElementById(this.id).value=document.getElementById(this.id).value.toUpperCase()">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                                <input id="password" type="password" class="form-control" placeholder="Ingrese contraseña">
-                            </div>
-
-                            
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Identidad</label>
-                                <input id="identidad" type="text" class="form-control" placeholder="Ingrese identidad">
-                            </div>
-                            
-                            <div class="mb-3">
-                            <label for="formGroupExampleInput" class="form-label">Genero</label>
-                            <select id="genero" class="form-select" aria-label="Default select example">
-                            <option selected>Seleccione un genero</option>
-                            <option value="1">Hombre</option>
-                            <option value="2">Mujer</option>
-                            </select>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Telefono</label>
-                                <input id="telefono" type="text" class="form-control" placeholder="Ingrese telefono">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Direccion</label>
-                                <input id="direccion" type="text" class="form-control" placeholder="Ingrese direccion">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="formGroupExampleInput" class="form-label">Correo</label>
-                                <input id="correo" type="text" class="form-control" placeholder="Ingrese correo electronico">
-                            </div>
-
-                            <div class="mb-3">
-                            <label for="formGroupExampleInput" class="form-label">Estado</label>
-                            <select id="estado" class="form-select" aria-label="Default select example">
-                            <option selected>Seleccione un estado</option>
-                            <option value="1">Activo</option>
-                            <option value="2">Inactivo</option>
-                            </select>
-                            </div>
-
-                            <div class="mb-3">
-                            <label for="formGroupExampleInput" class="form-label">Rol</label>
-                            <select id="rol" class="form-select" aria-label="Default select example">
-                            <option selected>Seleccione un rol</option>
-                            <option value="1">Administrador</option>
-                            <option value="2">Empleado</option>
-                            </select>
-                            </div>
-
-                            
-                            <!--FIN DATOS DEL MODAL--> 
-                     
-                            </div>                          
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancelar</button>
-                                <input name="btnguardar" class="btn btn-dark" title="click para guardar" type="submit" value="Guardar">
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                            
-                    </div>
-
+                        </tbody>
+                    </table>
+                    <!--BOTON PROVISIONAL SALIDA-->
+                    <input onclick="location.href='../../login.php'" name="" class="btn btn-danger" title="click para registrar un nuevo usuario" type="dark" value="Salir">
                 </div>
-
-            </main>
-        </div>
+                    
     </div>
-</form>
-
-
 
 
 
